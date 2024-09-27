f677357995eafd863c41bfd8fd8f2954125182ff
Said Suleiman Juma <said9923@gmail.com>
2024-09-27 13:06:06 +0300

    Added login script

--- lib/actions/auth/login.tsx
+++ lib/actions/auth/login.tsx
@@ -4,6 +4,8 @@ import {LoginSchema} from "@/types/data-schemas";
 import {z} from "zod";
 import {LoginResponse} from "@/types/types";
 import {parseStringify} from "@/lib/utils";
+import ApiClient from "@/lib/settlo-api-client";
+import {endpoints} from "@/types/endpoints";
 
 export const login = async (
     credentials: z.infer<typeof LoginSchema>,
@@ -19,12 +21,28 @@ export const login = async (
     }else{
         console.log("Submit to API", validatedData.data);
 
-        // call server
+        try {
+            const apiClient = new ApiClient();
+            const myEndpoints = endpoints();
+
+            const data = await apiClient.post(myEndpoints.auth.login.endpoint);
+
+            console.log("Action response", data);
+
+            return parseStringify({
+                type: "success",
+                data: data
+            });
+        } catch (error) {
+            throw error;
+        }
+
+        /*// call server
         return parseStringify({
             type: "success",
             password: validatedData.data?.password,
             username: validatedData.data?.username,
-        });
+        });*/
 
     }
 }
