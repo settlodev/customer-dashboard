feb194467f09f5399d61fd58f237c14606cf63ff
Said Suleiman Juma <said9923@gmail.com>
2024-10-01 15:38:32 +0300

    Added classnammes package for the landing page template

--- components/landing-page/utils/AppConfig.ts
+++ components/landing-page/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Settlo Customer Dashboard',
+  title: 'Settlo Customer Dashboard',
+  description: 'Settlo customer dashboard',
   locale: 'en',
 };
