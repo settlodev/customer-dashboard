800ba2403586f3d894af019e51334e08c55ecefb
Said Suleiman Juma <said9923@gmail.com>
2024-10-01 13:13:14 +0300

    Added google play and appstore icons

--- components/auth/authLayout.tsx
+++ components/auth/authLayout.tsx
@@ -6,39 +6,43 @@ interface Props {
 }
 
 export const AuthLayoutWrapper = ({ children }: Props) => {
-  return (
-    <div className='flex h-screen'>
-      <div className='flex-1 flex-col flex items-center justify-center p-6'>
-        <div className='md:hidden absolute left-0 right-0 bottom-0 top-0 z-0'>
-          <Image
-            className='w-full h-full'
-            src='https://nextui.org/gradients/docs-right.png'
-            alt='gradient'
-          />
-        </div>
-        {children}
-      </div>
+    return (
+        <div className='flex h-screen'>
+            <div className='flex-1 flex-col flex items-center justify-center p-6'>
+                <div className='md:hidden absolute left-0 right-0 bottom-0 top-0 z-0'>
+                    <Image
+                        className='w-full h-full'
+                        src='https://nextui.org/gradients/docs-right.png'
+                        alt='gradient'
+                    />
+                </div>
+                {children}
+            </div>
 
-      <div className='hidden my-10 md:block'>
-        <Divider orientation='vertical' />
-      </div>
+            <div className='hidden my-10 md:block'>
+                <Divider orientation='vertical'/>
+            </div>
 
-      <div className='hidden md:flex flex-1 relative flex items-center justify-center p-6'>
-        <div className='absolute left-0 right-0 bottom-0 top-0 z-0'>
-          <Image
-            className='w-full h-full'
-            src='https://nextui.org/gradients/docs-right.png'
-            alt='gradient'
-          />
-        </div>
+            <div className='hidden md:flex flex-1 relative flex items-center justify-center p-6'>
+                <div className='absolute left-0 right-0 bottom-0 top-0 z-0'>
+                    <Image
+                        className='w-full h-full'
+                        src='https://nextui.org/gradients/docs-right.png'
+                        alt='gradient'
+                    />
+                </div>
+                <div className='z-10'>
+                    <h1 className='font-bold text-[45px]'>Welcome to Settlo</h1>
+                    <div className='font-light text-slate-400 mt-4'>
+                        <p className="text-medium">Login with your credentials to continue using the Dashboard, Download
+                            Settlo App from <br/>Playstore and Appstore to use our POS in your android or iOS devices
+                        </p>
+                    </div>
+                    <div className='font-light text-slate-400 mt-4'>
 
-        <div className='z-10'>
-          <h1 className='font-bold text-[45px]'>Welcome to Settlo</h1>
-          <div className='font-light text-slate-400 mt-4'>
-            Login with your credentials to continue using the Dashboard
-          </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
