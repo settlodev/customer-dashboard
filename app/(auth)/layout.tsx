91e892cd99f47fba9d8557826d09478c66378aba
Said Suleiman Juma <said9923@gmail.com>
2024-10-01 12:37:45 +0300

    Defined menu items

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -12,24 +12,7 @@ export default function AuthLayout({
     return (
         <>
             <div className="container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-                {/*<Link*/}
-                {/*    href="/login"*/}
-                {/*    className={cn(*/}
-                {/*        buttonVariants({ variant: "ghost" }),*/}
-                {/*        "absolute right-4 top-4 md:right-8 md:top-8"*/}
-                {/*    )}*/}
-                {/*>*/}
-                {/*    Login*/}
-                {/*</Link>*/}
-                {/*<Link*/}
-                {/*    href="/register"*/}
-                {/*    className={cn(*/}
-                {/*        buttonVariants({ variant: "ghost" }),*/}
-                {/*        "absolute right-10 top-4 md:right-20 md:top-8"*/}
-                {/*    )}*/}
-                {/*>*/}
-                {/*    Register*/}
-                {/*</Link>*/}
+
                 <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
                     <div className="absolute inset-0 bg-zinc-900" />
                     <div className="relative z-20 flex items-center text-lg font-medium">
--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -16,9 +16,10 @@ import { CollapseItems } from "./collapse-items";
 import { SidebarItem } from "./sidebar-item";
 import { SidebarMenu } from "./sidebar-menu";
 import { FilterIcon } from "../icons/sidebar/filter-icon";
-import { useSidebarContext } from "../layout/layout-context";
+import { useSidebarContext } from "../layouts/layout-context";
 import { ChangeLogIcon } from "../icons/sidebar/changelog-icon";
 import { usePathname } from "next/navigation";
+import {DashboardIcon} from "@radix-ui/react-icons";
 
 export const SidebarWrapper = () => {
   const pathname = usePathname();
@@ -32,96 +33,86 @@ export const SidebarWrapper = () => {
       <div
         className={Sidebar({
           collapsed: collapsed,
-        })}
-      >
+        })}>
         <div className={Sidebar.Header()}>
           <CompaniesDropdown />
         </div>
         <div className="flex flex-col justify-between h-full">
           <div className={Sidebar.Body()}>
-            <SidebarItem
-              title="Home"
-              icon={<HomeIcon />}
-              isActive={pathname === "/"}
-              href="/"
-            />
-            <SidebarMenu title="Main Menu">
+
+            <SidebarMenu title="Dashbboard">
               <SidebarItem
-                isActive={pathname === "/accounts"}
-                title="Accounts"
-                icon={<AccountsIcon />}
-                href="accounts"
+                  isActive={pathname === "/summary"}
+                  title="Summary"
+                  icon={<DashboardIcon />}
+                  href="/summary"
               />
               <SidebarItem
-                isActive={pathname === "/payments"}
-                title="Payments"
-                icon={<PaymentsIcon />}
-              />
-              <CollapseItems
-                icon={<BalanceIcon />}
-                items={["Banks Accounts", "Credit Cards", "Loans"]}
-                title="Balances"
+                  isActive={pathname === "/top-selling"}
+                  title="Top Selling"
+                  icon={<DashboardIcon />}
+                  href="/top-selling"
               />
               <SidebarItem
-                isActive={pathname === "/customers"}
-                title="Customers"
-                icon={<CustomersIcon />}
+                  isActive={pathname === "/staff-report"}
+                  title="Staff Report"
+                  icon={<DashboardIcon />}
+                  href="/staff-report"
               />
+            </SidebarMenu>
+
+            <SidebarMenu title="Inventory">
               <SidebarItem
                 isActive={pathname === "/products"}
                 title="Products"
-                icon={<ProductsIcon />}
+                icon={<AccountsIcon />}
+                href="/products"
               />
               <SidebarItem
-                isActive={pathname === "/reports"}
-                title="Reports"
-                icon={<ReportsIcon />}
+                  isActive={pathname === "/suppliers"}
+                  title="Suppliers"
+                  icon={<CustomersIcon />}
+                  href="/suppliers"
               />
+              <SidebarItem
+                isActive={pathname === "/categories"}
+                title="Categories"
+                icon={<PaymentsIcon />}
+                href="/categories"
+              />
+              {/*<CollapseItems
+                icon={<BalanceIcon />}
+                items={["Banks Accounts", "Credit Cards", "Loans"]}
+                title="Balances"
+              />*/}
+
             </SidebarMenu>
 
             <SidebarMenu title="General">
               <SidebarItem
-                isActive={pathname === "/developers"}
-                title="Developers"
+                isActive={pathname === "/countries"}
+                title="Countries"
                 icon={<DevIcon />}
+                href='/countries'
               />
               <SidebarItem
-                isActive={pathname === "/view"}
-                title="View Test Data"
-                icon={<ViewIcon />}
+                  isActive={pathname === "/departments"}
+                  title="Departments"
+                  icon={<ProductsIcon />}
+                  href='/departments'
               />
               <SidebarItem
-                isActive={pathname === "/settings"}
-                title="Settings"
-                icon={<SettingsIcon />}
+                  isActive={pathname === "/discounts"}
+                  title="Discounts"
+                  icon={<ProductsIcon />}
+                  href='/discounts'
               />
             </SidebarMenu>
 
-            <SidebarMenu title="Updates">
-              <SidebarItem
-                isActive={pathname === "/changelog"}
-                title="Changelog"
-                icon={<ChangeLogIcon />}
-              />
-            </SidebarMenu>
           </div>
+
           <div className={Sidebar.Footer()}>
-            <Tooltip content={"Settings"} color="primary">
-              <div className="max-w-fit">
-                <SettingsIcon />
-              </div>
-            </Tooltip>
-            <Tooltip content={"Adjustments"} color="primary">
-              <div className="max-w-fit">
-                <FilterIcon />
-              </div>
-            </Tooltip>
-            <Tooltip content={"Profile"} color="primary">
-              <Avatar
-                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                size="sm"
-              />
-            </Tooltip>
+            <p className="text-xs">&copy; {(new Date()).getFullYear()} Settlo Tech Ltd</p>
           </div>
         </div>
       </div>
