e3423c51159c7622409d0800ac7eb35bb6106c33
pbijampola <patrickbijampola@gmail.com>
2024-10-22 17:01:50 +0300

    Dark mode issue

--- app/(auth)/business-registration/business_list.tsx
+++ app/(auth)/business-registration/business_list.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { useEffect } from "react"; // Import useEffect
 import { useRouter } from "next/navigation";
 import { Business } from "@/types/business/type";
 
 const CreatedBusinessList = ({ businesses }: { businesses: Business[]}) => {
     const router = useRouter();
-    if (businesses.length > 0) {
-        //router.push(`/business-location?business=${businesses[0].id}`);
-        router.push(`/select-business`);
-        return null; //preventing the table
-    }
+    
+    useEffect(() => { // Use useEffect for side effects
+        if (businesses.length > 0) {
+            //router.push(`/business-location?business=${businesses[0].id}`);
+            router.push(`/select-business`);
+        }
+    }, [businesses]); // Dependency array to run effect when businesses change
 
     return (<p>Loading...</p>)
 }
