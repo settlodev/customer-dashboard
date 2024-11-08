f16e94f9947566df0969446be02dbbe592401f46
pbijampola <patrickbijampola@gmail.com>
2024-11-08 11:11:45 +0300

    Fix: null useContext error

--- components/landing-page/Base.tsx
+++ components/landing-page/Base.tsx
@@ -1,5 +1,5 @@
-import { Meta } from './Meta';
-import { AppConfig } from './AppConfig';
+// import { Meta } from './Meta';
+// import { AppConfig } from './AppConfig';
 import {CallToAction } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
@@ -13,7 +13,7 @@ import LandingPageNav from "./LandingPageNav";
 const Base = () => {
     return (
     <div className="flex flex-col items-center justify-center w-full antialiased max-w-[1280px] mx-auto">
-        <Meta title={AppConfig.title} description={AppConfig.description} />
+        {/* <Meta title={AppConfig.title} description={AppConfig.description} /> */}
         <LandingPageNav />
         <Hero />
         <Sponsors />
