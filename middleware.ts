9eec3b6f33cea82d79831a1c0723022689c4e41a
pbijampola <patrickbijampola@gmail.com>
2024-12-10 16:22:30 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -92,7 +92,7 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
-    "user-verification",
-    "email-verification"
+    "/user-verification",
+    "/email-verification"
   ],
 };
