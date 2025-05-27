f0aefe6b74053410ee34c5775a853d3937486c95
pbijampola <patrickbijampola@gmail.com>
2025-05-27 14:16:53 +0300

    Update companies-dropdown.tsx

--- components/sidebar/companies-dropdown.tsx
+++ components/sidebar/companies-dropdown.tsx
@@ -36,8 +36,6 @@ export const CompaniesDropdown = ({ data }: { data: BusinessPropsType }) => {
 
     const handleLocationSelect = (location: Location) => {
         const isActive = currentLocation?.id === location.id;
-
-        console.log("Selected location:", location);
         
         if (isActive) return;
         
