a90c1b469f326b8f97779033b58064132b3e17fe
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2025-06-09 14:13:40 +0300

    Update schema.ts

--- types/location/schema.ts
+++ types/location/schema.ts
@@ -2,21 +2,21 @@ import { isValidPhoneNumber } from "libphonenumber-js";
 import {boolean, object, string} from "zod";
 
 export const LocationSchema = object({
-    name: string().min(2, 'Name of location must be more than 3 characters').max(50, 'Name of location can not be more than 50 characters'),
-    phone: string().min(8, 'Phone number must be more than 8 characters').max(20, 'Phone number can not be more than 20 characters').refine(isValidPhoneNumber, {
+    name: string({ required_error: "Name is required" }).min(3, "Name should be at least 3 characters").max(50, "Name can not be more than 50 characters"),
+    phone: string({ required_error: "Phone number is required" }).min(8, 'Phone number must be more than 8 characters').max(20, 'Phone number can not be more than 20 characters').refine(isValidPhoneNumber, {
         message: "Invalid phone number",
     }),
-    email: string().email("Please enter a valid email address"),
-    description: string().min(2, 'Description must be more than 2 characters').max(120, 'Description can not be more than 120 characters').optional().nullish(),
-    address: string().min(2, 'Address must be more than 20 characters').max(120, 'Address can not be more than 0 characters'),
-    city: string(),
+    email: string({ required_error: "Email is required" }).email("Please enter a valid email address"),
+    description: string().optional(),
+    address: string({ required_error: "Address is required" }),
+    city: string({ required_error: "City is required" }),
     region: string().optional().nullish(),
-    street: string().optional().nullish(),
-    openingTime: string(),
-    closingTime: string(),
-    status: boolean().optional().nullish(),
-    business: string().optional().nullish(),
-    image: string().optional().nullish(),
+    street: string().optional(),
+    openingTime: string({ required_error: "Opening time is required" }),
+    closingTime: string({ required_error: "Closing time is required" }),
+    status: boolean().optional(),
+    // business: string().optional().nullish(),
+    // image: string().optional().nullish(),
    
     subscription: string().optional().nullish()
 })
