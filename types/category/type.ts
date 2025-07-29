7765994fdb05586171863072681576d40284bda2
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2025-07-29 13:00:00 +0300

    Fix: stock quantity on product table

--- types/category/type.ts
+++ types/category/type.ts
@@ -5,8 +5,12 @@ export declare interface Category {
     "name": string,
     "image": string,
     "parentCategory": UUID,
+    "parentCategoryName":string
     "status": boolean,
     "canDelete": boolean,
     "location": UUID,
+    "locationName":string
+    "productCount":number
+    "productVariantsCount":number
     "isArchived": boolean
 }
