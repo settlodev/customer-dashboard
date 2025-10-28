88c4cad965441e73d19369e378f3f539bf37a2bf
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2025-10-28 15:37:29 +0300

    feat: improve sold items dashboard metrics organization and responsive design

--- types/product/type.ts
+++ types/product/type.ts
@@ -1,81 +1,86 @@
-import { UUID } from "crypto"
-import {Variant} from "@/types/variant/type";
+import { UUID } from "crypto";
+import { Variant } from "@/types/variant/type";
 
 export declare interface Product {
-    id: UUID,
-    name: string,
-    slug: string,
-    sku: string,
-    image: string,
-    description: string,
-    color: string,
-    sellOnline: boolean,
-    status: boolean,
-    canDelete: boolean,
-    business: UUID,
-    category: UUID,
-    categoryName:string,
-    location: UUID,
-    department: UUID,
-    departmentName: string,
-    brand: UUID,
-    brandName: string,
-    taxClass:string
-    taxIncluded:boolean
-    isArchived: boolean,
-    variants: Variant[],
-    trackInventory: boolean,
-    trackingType: "recipe" | "stock" | null,
-    quantity:number
+  id: UUID;
+  name: string;
+  slug: string;
+  sku: string;
+  image: string;
+  description: string;
+  color: string;
+  sellOnline: boolean;
+  status: boolean;
+  canDelete: boolean;
+  business: UUID;
+  category: UUID;
+  categoryName: string;
+  location: UUID;
+  department: UUID;
+  departmentName: string;
+  brand: UUID;
+  brandName: string;
+  taxClass: string;
+  taxIncluded: boolean;
+  isArchived: boolean;
+  variants: Variant[];
+  trackInventory: boolean;
+  trackingType: "recipe" | "stock" | null;
+  quantity: number;
 }
 
-export declare interface TopSellingProduct{
-    startDate: Date,
-    endDate: Date,
-    locationName: string,
-    items: TopItems[]
-    totalRevenue: number,
-    totalQuantitySold: number,
-    soldItemsReport: SoldItemsReport
+export declare interface TopSellingProduct {
+  startDate: Date;
+  endDate: Date;
+  locationName: string;
+  items: TopItems[];
+  totalRevenue: number;
+  totalQuantitySold: number;
+  soldItemsReport: SoldItemsReport;
 }
 
-export interface TopItems{
-    name:string,
-    productName: string,
-    variantName: string,
-    categoryName: string,
-    imageUrl:string,
-    staffName:string,
-    quantity: number,
-    revenue: number
-    percentageOfTotal: number
-    averagePrice: number
-    latestSoldDate:Date
-    earliestSoldDate:Date
+export interface TopItems {
+  name: string;
+  productName: string;
+  variantName: string;
+  categoryName: string;
+  imageUrl: string;
+  staffName: string;
+  quantity: number;
+  revenue: number;
+  percentageOfTotal: number;
+  averagePrice: number;
+  latestSoldDate: Date;
+  earliestSoldDate: Date;
 }
 export interface SoldItemsReport {
-    locationName: string;
-    startDate: Date;
-    endDate: Date;
-    items: SoldItem[]; // Changed to an array of SoldItem
-    totalQuantity: number;
-    totalRevenue: number;
-    totalCost: number | null; // Assuming cost can be null
-    totalProfit: number;
-    averageMargin: number;
+  locationName: string;
+  startDate: Date;
+  endDate: Date;
+  items: SoldItem[];
+  totalQuantity: number;
+  totalRevenue: number;
+  totalCost: number | null;
+  totalProfit: number;
+  averageMargin: number;
 }
-export interface SoldItem{
-    productName: string,
-    variantName: string,
-    categoryName: string,
-    staffName: string,
-    imageUrl: string
-    quantity: number,
-    price: number,
-    cost: number,
-    profit: number,
-    margin: number,
-    latestSoldDate: string
-    earliestSoldDate: string
+export interface SoldItem {
+  productName: string;
+  variantName: string;
+  categoryName: string;
+  staffName: string;
+  imageUrl: string;
+  quantity: number;
+  price: number;
+  cost: number;
+  profit: number;
+  margin: number;
+  refundedQuantity: number;
+  refundedPrice: number;
+  netPrice: number;
+  refundedCost: number;
+  netQuantity: number;
+  netCost: number;
+  latestSoldDate: string;
+  earliestSoldDate: string;
 }
-
