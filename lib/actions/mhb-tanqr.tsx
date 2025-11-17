e6ace11d2b9f1fc49de2b1cff079bca551b5f85c
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2025-11-17 16:45:28 +0300

    deleted file

--- lib/actions/mhb-tanqr.tsx
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import ApiClient from "@/lib/settlo-api-client";
-import { getAuthenticatedUser } from "@/lib/auth-utils";
-import { parseStringify } from "@/lib/utils";
-import { EmploymentDetailsSchema } from "@/types/tanqr/schema";
-import { z } from "zod";
-import { FormResponse } from "@/types/types";
-import { getCurrentLocation } from "@/lib/actions/business/get-current-business";
