b90f080787a3d8a0db7c73a5a4755ccc3d25c309
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2026-02-09 15:40:45 +0300

    Clean up RecipeSelector by removing unused code
    Removed commented-out code for RecipeSelector component.

--- components/widgets/recipe-selector.tsx
+++ components/widgets/recipe-selector.tsx
@@ -1,72 +1,3 @@
-// import { Recipe } from "@/types/recipe/type";
-// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
-// import { useEffect, useState } from "react";
-// import { fetchRecipes } from "@/lib/actions/recipe-actions";
-
-// interface RecipeProps {
-//     label?: string;
-//     placeholder: string;
-//     isRequired?: boolean;
-//     value?: string;
-//     isDisabled?: boolean;
-//     description?: string;
-//     onChange: (value: string) => void;
-//     onBlur?: () => void;
-// }
-// function RecipeSelector({
-//     placeholder,
-//     value,
-//     isDisabled,
-//     onChange,
-// }: RecipeProps) {
-//     const [recipes, setRecipes] = useState<Recipe[]>([]);
-//     const [isLoading, setIsLoading] = useState<boolean>(true);
-
-//     useEffect(() => {
-//         async function loadRecipes() {
-//             try {
-//                 setIsLoading(true);
-//                 const fetchedRecipes = await fetchRecipes();
-//                 setRecipes(fetchedRecipes);
-//             } catch (error: any) {
-//                 console.log("Error fetching recipes:", error);
-//             } finally {
-//                 setIsLoading(false);
-//             }
-//         }
-//         loadRecipes();
-//     }, []);
-//     return (
-        
-//         <div className="space-y-2">
-//         <Select
-//             defaultValue={value}
-//             disabled={isDisabled || isLoading}
-//             value={value}
-//             onValueChange={onChange}
-//         >
-//             <SelectTrigger className="w-full">
-//                 <SelectValue
-//                     placeholder={placeholder || "Select department"}
-//                 />
-//             </SelectTrigger>
-//             <SelectContent>
-//                 {recipes.map((recipe) => (
-//                     <SelectItem
-//                         key={recipe.id}
-//                         value={recipe.id}
-//                     >
-//                         {recipe.name}
-//                     </SelectItem>
-//                 ))}
-//             </SelectContent>
-//         </Select>
-        
-//     </div>
-//     )
-// }
-// export default RecipeSelector
-
 "use client";
 
 import React, {
@@ -399,4 +330,3 @@ const StockVariantSelector: React.FC<Props> = ({
 };
 
 export default StockVariantSelector;
-
